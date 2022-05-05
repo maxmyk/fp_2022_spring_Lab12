26e6735bf730e6d7ccb6b81335826667fb9a19ef
Maksym Mykhasiuta <maksym.mykhasiuta@ucu.edu.ua>
2022-05-05 11:44:19 +0300

    Starter

---
+++ queue_to_stack.py
@@ -0,0 +1,10 @@
+"""
+Queue to stack converter.
+"""
+
+from arrayqueue import ArrayQueue    # or from linkedqueue import LinkedQueue
+from arraystack import ArrayStack    # or from linkedstack import LinkedStack
+
+
+def queue_to_stack(queue):
+    pass
---
+++ stack_to_queue.py
@@ -0,0 +1,10 @@
+"""
+Stack to queue converter.
+"""
+
+from arraystack import ArrayStack   # or from linkedstack import LinkedStack
+from arrayqueue import ArrayQueue   # or from linkedqueue import LinkedQueue
+
+
+def stack_to_queue(stack):
+    pass
