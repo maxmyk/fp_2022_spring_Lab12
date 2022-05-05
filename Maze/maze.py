e6aa45ac84d4fefc104d5c09d058e564931911b2
Maksym Mykhasiuta <maksym.mykhasiuta@ucu.edu.ua>
2022-05-05 13:59:34 +0300

    directory renamed

--- Maze/maze.py
+++ Maze/maze.py
@@ -26,21 +26,20 @@ class Maze:
     def set_wall(self, row, col):
         """Fills the indicated cell with a "wall" marker."""
         assert row >= 0 and row < self.num_rows() and \
-               col >= 0 and col < self.num_cols(), "Cell index out of range."
+            col >= 0 and col < self.num_cols(), "Cell index out of range."
         self._maze_cells[row, col] = self.MAZE_WALL
 
     def set_start(self, row, col):
         """Sets the starting cell position."""
         assert row >= 0 and row < self.num_rows() and \
-               col >= 0 and col < self.num_cols(), "Cell index out of range."
+            col >= 0 and col < self.num_cols(), "Cell index out of range."
         self._start_cell = _CellPosition(row, col)
 
     def set_exit(self, row, col):
         """Sets the exit cell position."""
         assert row >= 0 and row < self.num_rows() and \
-               col >= 0 and col < self.num_cols(), "Cell index out of range."
+            col >= 0 and col < self.num_cols(), "Cell index out of range."
         self._exit_cell = _CellPosition(row, col)
-
 
     def find_path(self):
         """
@@ -60,8 +59,8 @@ class Maze:
     def _valid_move(self, row, col):
         """Returns True if the given cell position is a valid move."""
         return row >= 0 and row < self.num_rows() \
-               and col >= 0 and col < self.num_cols() \
-               and self._maze_cells[row, col] is None
+            and col >= 0 and col < self.num_cols() \
+            and self._maze_cells[row, col] is None
 
     def _exit_found(self, row, col):
         """Helper method to determine if the exit was found."""
@@ -78,6 +77,73 @@ class Maze:
 
 class _CellPosition(object):
     """Private storage class for holding a cell position."""
+
     def __init__(self, row, col):
         self.row = row
         self.col = col
+
+
+def build_maze(filename):
+    """Builds a maze based on a text format in the given file."""
+    infile = open(filename, "r")
+
+    # Read the size of the maze.
+    nrows, ncols = read_value_pair(infile)
+    maze = Maze(nrows, ncols)
+
+    # Read the starting and exit positions.
+    row, col = read_value_pair(infile)
+    maze.set_start(row, col)
+    row, col = read_value_pair(infile)
+    maze.set_exit(row, col)
+
+    # Read the maze itself.
+    for row in range(nrows):
+        line = infile.readline()
+        for col in range(len(line)):
+            if line[col] == "*":
+                maze.set_wall(row, col)
+
+    # Close the maze file and return the newly constructed maze.
+    infile.close()
+
+    return maze
+
+
+def read_value_pair(infile):
+    """Extracts an integer value pair from the given input file."""
+    line = infile.readline()
+    valA, valB = line.split()
+    return int(valA), int(valB)
+
+
+def main():
+    """
+    >>> maze = build_maze("Maze/mazefile.txt")
+    >>> print(maze)
+    * * * * * 
+    * _ * _ * 
+    * _ _ _ * 
+    * _ * _ _ 
+    _ _ * * * 
+    >>> maze.findPath()
+    True
+    >>> print(maze)
+    * * * * * 
+    * o * o * 
+    * x x x * 
+    * x * x x 
+    _ x * * * 
+    >>> maze.reset()
+    >>> print(maze)
+    * * * * * 
+    * _ * _ * 
+    * _ _ _ * 
+    * _ * _ _ 
+    _ _ * * *
+    """
+
+
+if __name__ == '__main__':
+    import doctest
+    print(doctest.testmod())
